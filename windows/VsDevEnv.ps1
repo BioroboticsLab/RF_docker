01c2d70c16694d0a85b2c49e7dd5b0a86a8c727f
Moritz Maxeiner <moritz@ucworks.org>
2018-09-16 13:33:08 +0200

    Configurable dev env for Windows

--- windows/VsDevEnv.ps1
+++ windows/VsDevEnv.ps1
@@ -1,5 +1,5 @@
 pushd "C:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/Common7/Tools"
-cmd /c "VsDevCmd.bat&set" |
+cmd /c "VsDevCmd.bat $args & set" |
 foreach {
   if ($_ -match "=") {
     $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
