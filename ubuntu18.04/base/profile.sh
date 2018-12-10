26ec423123dc6fcfa2ff2082b8e4f6c5521eb88a
Moritz Maxeiner <mm@ucw.sh>
2018-12-10 03:58:06 +0100

    ubuntu18.04: Add some CI helpers

---
+++ ubuntu18.04/base/profile.sh
@@ -0,0 +1,10 @@
+function gitlab-fetch-artifacts {
+    curl -o artifacts.zip --header "JOB-TOKEN: $CI_JOB_TOKEN" "https://git.imp.fu-berlin.de/api/v4/projects/$1/jobs/artifacts/$2/download?job=$3"
+    7z e artifacts.zip; rm artifacts.zip
+}
+
+function cmake-integrate-package {
+    7z e $1-*.tar.xz; rm $1-*.tar.xz
+    7z x $1-*.tar; rm $1-*.tar
+    ln -s $(realpath $1-*) /usr/local/$1
+}
