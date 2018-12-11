fc318575443cb073346bb627ed9a1e6ba887768d
Moritz Maxeiner <mm@ucw.sh>
2018-12-11 11:19:36 +0100

    Add trigger helper

--- ubuntu18.04/base/profile.sh
+++ ubuntu18.04/base/profile.sh
@@ -3,6 +3,10 @@ function gitlab-fetch-artifacts {
     7z e artifacts.zip; rm artifacts.zip
 }
 
+function gitlab-trigger {
+    curl --request POST --form "token=$2" --form "ref=$3" "https://git.imp.fu-berlin.de/api/v4/projects/$1/trigger/pipeline"
+}
+
 function cmake-integrate-package {
     7z e $1-*.tar.xz; rm $1-*.tar.xz
     7z x $1-*.tar; rm $1-*.tar
